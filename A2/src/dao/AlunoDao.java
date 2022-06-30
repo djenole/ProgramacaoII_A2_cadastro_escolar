c6d10d33c59506e18fb9126ad9e1a6b6d4487cbb
Djenole Reis <djenole@gmail.com>
2022-06-30 16:36:52 -0300

    Atualizado import da factory e adicionado exception no método insert

--- A2/src/dao/AlunoDao.java
+++ A2/src/dao/AlunoDao.java
@@ -1,24 +1,28 @@
 package dao;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
-
+import java.sql.SQLException;
+import banco.Factory;
 import modelo.Aluno;
 
-public class AlunoDao {
-//	Connection con;
 
-//	AlunoDao() throws SQLException {
-//	con = Factory.getConnection();
+public class AlunoDao {
+	private Connection con;
+
+	public AlunoDao() throws SQLException {
+		con = Factory.getConnection();
+	}
 	
-	public void inser(Aluno aluno) {
+	public void insert(Aluno aluno) throws SQLException {
 		String query = "insert into aluno (nome, email, cpf, dataNasc, naturalidade, endereco) values (?,?,?,?,?,?)";
 		PreparedStatement stmt = con.prepareStatement(query);
-		stmt.setString(aluno.getNome());
-		stmt.setString(aluno.getEmail());
-		stmt.setString(aluno.getCpf());
-		stmt.setString(aluno.getDataNasc());
-		stmt.setString(aluno.getNaturalidade());
-		stmt.setString(aluno.getEndereco());
+		stmt.setString(1, aluno.getNome());
+		stmt.setString(2, aluno.getEmail());
+		stmt.setString(3, aluno.getCpf());
+		stmt.setString(4, aluno.getDataNasc());
+		stmt.setString(5, aluno.getNaturalidade());
+		stmt.setString(6, aluno.getEndereco());
 		stmt.execute();
 		stmt.close();
 		con.close();
