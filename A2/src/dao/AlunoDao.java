85737c57012d92f6eb6e82f1a9cd725cfdc64122
Djenole Reis <djenole@gmail.com>
2022-06-30 17:50:06 -0300

    acrescentado métodos de listagem, listagem com filtro e atualização de aluno

--- A2/src/dao/AlunoDao.java
+++ A2/src/dao/AlunoDao.java
@@ -2,7 +2,11 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import banco.Factory;
 import modelo.Aluno;
 
@@ -20,14 +24,82 @@ public class AlunoDao {
 		stmt.setString(1, aluno.getNome());
 		stmt.setString(2, aluno.getEmail());
 		stmt.setString(3, aluno.getCpf());
-		stmt.setString(4, aluno.getDataNasc());
+		stmt.setDate(4, aluno.getDataNasc());
 		stmt.setString(5, aluno.getNaturalidade());
 		stmt.setString(6, aluno.getEndereco());
 		stmt.execute();
 		stmt.close();
 		con.close();
-		
-	
 	}
+	
+	public List<Aluno> getList() throws SQLException{
+		String query = "select * from alunos";
+		PreparedStatement stmt = con.prepareStatement(query);
+		ResultSet rset = stmt.executeQuery();
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		
+		while(rset.next()) {
+			Aluno aluno = new Aluno();
+			aluno.setNome(rset.getString("nome"));
+			aluno.setEmail(rset.getString("email"));
+			aluno.setCpf(rset.getString("CPF"));
+			aluno.setDataNasc(rset.getDate(query, null));
+			aluno.setNaturalidade(rset.getString("naturalidade"));
+			aluno.setEndereco(rset.getString("endereco"));
+			alunos.add(aluno);
+		}
+		rset.close();
+		stmt.close();
+		con.close();
+		
+		return alunos;
+		
+	}
+	
+	public List<Aluno> getFilterTheListByTheFirstLetter(char letra) throws SQLException {
+		String query = "select * from contatos where nome like '"+letra+"%'";
+		PreparedStatement stmt = con.prepareStatement(query);
+		ResultSet rset = stmt.executeQuery();
+		
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		
+		while(rset.next()) {
+			Aluno aluno = new Aluno();
+			aluno.setNome(rset.getString("nome"));
+			aluno.setEmail(rset.getString("email"));
+			aluno.setCpf(rset.getString("cpf"));
+			aluno.setDataNasc(rset.getDate(query, null));
+			aluno.setNaturalidade(rset.getString("naturalidade"));
+			aluno.setEndereco(rset.getString("endereco"));
+			
+			alunos.add(aluno);
+		}
+		rset.close();
+		stmt.close();
+		con.close();
+		
+		return alunos;
+	}
+	
+	public void updateAluno(Aluno aluno, Long id) throws SQLException {
+		String sql = "UPDATE CONTATOS SET NOME = ?, EMAIL = ?, CPF = ?, DATA = ?, NATURALIDADE = ?, ENDERECO = ? WHERE ID = "+id+"";
+		
+		PreparedStatement preparedStatement = con.prepareStatement(sql);
+		
+		preparedStatement.setString(1, aluno.getNome());
+		preparedStatement.setString(2, aluno.getEmail());
+		preparedStatement.setString(3, aluno.getCpf());
+		preparedStatement.setDate(4, aluno.getDataNasc());
+		preparedStatement.setString(5, aluno.getNaturalidade());
+		preparedStatement.setString(6, aluno.getEndereco());
+		preparedStatement.execute();
+		preparedStatement.close();
+		
+		
+		
+	}
+	
+	
+	
 
 }
